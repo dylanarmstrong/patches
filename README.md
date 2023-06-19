03460257446e76c7f849ae37681e699e345d4d87
Dylan Armstrong <dylan@dylan.is>
2023-06-19 11:53:40 +0200

    feat: ck3 checksum disable added

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 ## Binary Patches
 
+### Note
+
+Only tested on MacOS
+
+### Run
+
+```bash
+cd ~/Library/Application Support/Steam/steamapps/common/Crusader Kings III/binaries/ck3.app/Contents/MacOS
+python3 /path/to/ck3.py
+```
+
 ### Compilation
 `gcc patch.c -o patch && ./patch file`
+
+### ck3.py
+Disable checksum validation (Working on 1.9.2)
 
 ### eu4.c
 Disable checksum validation (Not working as of 2018-09-25)
---
+++ ck3.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+###              ###
+#   CK3 Patcher    #
+#  Version 1.9.2   #
+###              ###
+
+filename = "./ck3"
+
+
+def patch(offset, replace):
+    with open(filename, "r+b") as f:
+        f.seek(offset)
+        f.write(replace)
+
+
+# Patch checksum, so custom UI mods do not disable Ironman
+def patch_checksum(s):
+    # Looking for OnChecksumCalculated
+    find = b"\x53\x48\x83\xec\x28\x48\x8d\x05\xa4\x16\x8e\x05"
+    offset = s.find(find) + 1
+    if offset > 1:
+        # Replace with a jmp to return immediately, as this method only sets if value doesn't match
+        print("Patching checksum offset: {}".format(hex(offset)))
+        patch(offset, b"\xe9\x75\x00\x00\x00")
+    else:
+        # Already patched
+        find = b"\x53\xe9\x75\x00\x00\x00\x8d"
+        offset = s.find(find)
+        if offset > 0:
+            print("Cannot patch checksum, already patched")
+        else:
+            print("Cannot patch checksum, unsupported version")
+
+
+def main():
+    s = None
+    with open(filename, "rb") as f:
+        s = f.read()
+    if s is None:
+        print("Cannot read {}".format(filename))
+        exit(1)
+    else:
+        patch_checksum(s)
+
+
+if __name__ == "__main__":
+    main()
